642a817a5ab248c398071526891224bf0fca8962
Xuân Trường <alonesera1612@gmail.com>
2025-07-11 00:28:01 +0700

    Create deleteFront.c

---
+++ LinkedList/SingleLinkedList/deleteFront.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node;
+
+node *makeNode(int x)
+{
+    node *newNode = (node *)malloc(sizeof(node));
+    newNode->data = x;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void print(node *head)
+{
+    while (head != NULL)
+    {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+void deleFront(node **head)
+{
+    if (*head == NULL)
+        return;
+    else
+    {
+        node *tmp = *head;
+        *head = (*head)->next;
+        free(tmp);
+    }
+}
+
+int main()
+{
+    node *head = NULL;
+    int n, m;
+    scanf("%d%d", &n, &m);
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        scanf("%d", &x);
+        node *newNode = makeNode(x);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        deleFront(&head);
+    }
+    print(head);
+
+    return 0;
+}
