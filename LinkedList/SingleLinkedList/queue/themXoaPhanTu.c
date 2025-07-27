addf7bb59bf317de5e5c72de95b97208d2651dec
VŨ XUÂN TRƯỜNG <alonesera1612@gmail.com>
2025-07-27 21:05:06 +0700

    Update themXoaPhanTu.c

--- LinkedList/SingleLinkedList/queue/themXoaPhanTu.c
+++ LinkedList/SingleLinkedList/queue/themXoaPhanTu.c
@@ -1,72 +1,55 @@
-// class
-
-
------------------------------------------------------------
-  
 #include <stdio.h>
 
 #define MAX 100000
 
-int queue[MAX];
-
-int isEmpty(){ return front > end;}
-
-int isFull(){ return end == MAX - 1;}
-
-void createQueue(int *front, int *end, int *count){
+void createQueue(int *front, int *rear, int *count){
     *front = 0;
-    *end = -1;
+    *rear = 0;
     *count = 0;
 }
 
-void enqueue(int Arr[], int x, int *front, int *end){
-    if(*count < MAX){
-        if (isFull()){
-            *end = 0;
-        }
-        else (*front)++;
-        (*count)++;
-    }
-    else printf("Queue Full");
+void enqueue(int *rear, int *count, int arr[], int x){
+    if (*count == MAX) return; //full
+    arr[*rear] = x;
+    *rear = (*rear + 1) % MAX;
+    (*count)++;
 }
 
-int dequeue(int Queue[], int *end, int count){
-    if(count != 0){
-        int tmp = Arr[*end];
-        if (*end == MAX - 1){
-            *end= 0;
-        }
-        else (*end)++;
-        count--;
-        return tmp;
+int dequeue(int *front, int *count, int arr[]){
+    if (*count == 0) return -1; // empty
+    int tmp = arr[*front];
+    *front = (*front + 1) % MAX;
+    (*count)--;
+    return tmp;
+}
+
+void printQueue(int front, int count, int arr[]){
+    for (int i = 0; i < count; i++){
+        int idx = (front + i) % MAX;
+        printf("%d ", arr[idx]);
     }
-    else {
-        printf ("Queue Empty");
-        return -1;
+    printf("\n");
+}
+
+void action(int *front, int *rear, int *count, int arr[]){
+    int t; scanf("%d", &t);
+    if (t == 1) dequeue(front, count, arr);
+    else if (t == 2) {
+        int x; scanf("%d", &x);
+        enqueue(rear, count, arr, x);
     }
 }
 
-int printQueue(int Arr[], int front, int count){
-    for(int i = 0; i < count; i++){
-        
-    }
-}
+int main (){
+    int arr[MAX], n, f, r, c;
+    scanf("%d", &n);
 
-int main(){
-    int Arr[MAX];
-    int f, e, c, n;
-    createQueue(&f, &e, &c);
-    for(int i = 0; i < n; i++){
-        int action; scanf("%d", &action);
-        if(action == 1){
-            dequeue(Arr, &f, &e);
-        }
-        else if(action == 2){
-            int u; scanf(" %d", &u);
-            enqueue(Arr, u, &f, &e);
-        }
-    }
-    
-    
+    createQueue(&f, &f, &c);
+
+    for (int i = 0; i < n; i++)
+        action(&f, &r, &c, arr);
+
+    printQueue(f, c, arr);
+
     return 0;
 }
