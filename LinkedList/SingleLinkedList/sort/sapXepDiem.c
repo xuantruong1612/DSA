20efecfa661962a5d408b36285aa2b946a0d06ee
Xuân Trường <alonesera1612@gmail.com>
2025-08-01 08:27:37 +0700

    Update sapXepDiem.c

--- LinkedList/SingleLinkedList/sort/sapXepDiem.c
+++ LinkedList/SingleLinkedList/sort/sapXepDiem.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <math.h>
 
 typedef struct {
     int x, y;
     int index;
 } Point;
 
-int khoangCachBinhPhuong(Point p) {
-    return p.x * p.x + p.y * p.y;
+long long khoangCachBinhPhuong(Point p) {
+    return (long long)p.x * p.x + (long long)p.y * p.y;
 }
 
 void swap(Point *a, Point *b) {
@@ -17,24 +16,22 @@ void swap(Point *a, Point *b) {
 }
 
 int compare(Point a, Point b) {
-    int da = khoangCachBinhPhuong(a);
-    int db = khoangCachBinhPhuong(b);
+    long long da = khoangCachBinhPhuong(a);
+    long long db = khoangCachBinhPhuong(b);
     if (da != db)
-        return da - db;
+        return da < db ? -1 : 1;
     return a.index - b.index;
 }
 
 int partition(Point arr[], int low, int high) {
     Point pivot = arr[high];
     int i = low - 1;
-
     for (int j = low; j < high; j++) {
         if (compare(arr[j], pivot) <= 0) {
             i++;
             swap(&arr[i], &arr[j]);
         }
     }
-
     swap(&arr[i + 1], &arr[high]);
     return i + 1;
 }
@@ -42,7 +39,6 @@ int partition(Point arr[], int low, int high) {
 void quickSort(Point arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
-
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
@@ -52,7 +48,6 @@ int main() {
     int n;
     scanf("%d", &n);
     Point points[n];
-
     for (int i = 0; i < n; i++) {
         scanf("%d %d", &points[i].x, &points[i].y);
         points[i].index = i;
